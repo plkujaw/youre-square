28e3e46c1017dbeff88982ece44da4b7abab8c97
plkujaw <jawor.jakub@gmail.com>
2020-05-29 10:52:55 +0100

    tidied up code

--- lib/square.rb
+++ lib/square.rb
@@ -1,11 +1,8 @@
 def is_square(number)
-
-if number < 0
-  return false
-end
-
-  p check = Math.sqrt(number).to_s.split('.')
-  p check[1].to_i == 0
-
-
+  if number < 0
+    false
+  else
+    check = Math.sqrt(number).to_s.split('.')
+    check[1].to_i == 0
   end
+end
