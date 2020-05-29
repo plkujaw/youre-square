675bdec4bda3438da8bc91d1152114cbc01741e4
plkujaw <jawor.jakub@gmail.com>
2020-05-29 09:42:52 +0100

    write and pass first test for 2

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -4,4 +4,7 @@ describe '#is_square' do
   it 'returns true if given number is the square number' do
     expect(is_square(2)).to eq(true)
   end
+  it 'returns false if given nmber is not the square number' do
+      expect(is_square(1)).to eq(false)
+  end
 end
